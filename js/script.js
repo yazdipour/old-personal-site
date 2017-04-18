0cda637bb108151fa6359169a4bb799327fd3157
Shahriar <code@shahriar.in>
2017-04-18 09:26:15 +0430

    change bg + remove fadeIn effect

--- js/script.js
+++ js/script.js
@@ -61,19 +61,19 @@ else
 {
 
   // FadeTo elements
-  if ( $(window).width() > 1023) {  
+  // if ( $(window).width() > 1023) {  
 
-    tiles = $("h2, h3, .column-one, .column-two, .column-three, .grid li, .contact .content .form, .contact .content .contact-text ").fadeTo(0, 0);
+  //   tiles = $("h2, h3, .column-one, .column-two, .column-three, .grid li, .contact .content .form, .contact .content .contact-text ").fadeTo(0, 0);
 
-    $(window).scroll(function(d,h) {
-      tiles.each(function(i) {
-          a = $(this).offset().top + $(this).height();
-          b = $(window).scrollTop() + $(window).height();
-          if (a < b) $(this).fadeTo(1000,1);
-      });
-    });
+  //   $(window).scroll(function(d,h) {
+  //     tiles.each(function(i) {
+  //         a = $(this).offset().top + $(this).height();
+  //         b = $(window).scrollTop() + $(window).height();
+  //         if (a < b) $(this).fadeTo(1000,1);
+  //     });
+  //   });
 
-  }
+  // }
 
 }
 
